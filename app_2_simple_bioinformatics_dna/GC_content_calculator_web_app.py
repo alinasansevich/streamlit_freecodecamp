7c7e5dd46f5467d5d908d2f64a57a92a386f33e9
Alina Sansevich <alinasansevich@gmail.com>
2021-02-05 11:38:18 -0600

    Almost ready.

--- app_2_simple_bioinformatics_dna/GC_content_calculator_web_app.py
+++ app_2_simple_bioinformatics_dna/GC_content_calculator_web_app.py
@@ -6,6 +6,8 @@ Created on Mon Jan 11 14:16:09 2021
 @author: alina
 
 GC Content Calculator Web App
+
+Idea from: https://www.youtube.com/watch?v=JwSS70SZdyM
 """
 import numpy as np
 import pandas as pd
@@ -13,25 +15,77 @@ import streamlit as st
 import altair as alt
 from PIL import Image
 
-# Page Title CONTINUE LATER XXXXXXXXXXXXXXXXXXXX
+
+# Count(G + C)/Count(A + T + G + C) * 100%
+def gc_content(window):
+    """
+    (str) -> int
+    Returns the GC content of window (a DNA or RNA sequence).
+    """
+    gc = window.count('G') + window.count('C')
+    atgc = window.count('A') + window.count('T') + window.count('G') + window.count('C')
+    
+    return (gc/atgc) * 100
+
+
+def DNA_nucleotide_count(seq):
+    """
+    (str) -> dict
+    Returns a dictionary with the nucleotide frequency for sequence seq.
+    """
+    d = dict([
+        ('A',seq.count('A')),
+        ('T',seq.count('T')),
+        ('G',seq.count('G')),
+        ('C',seq.count('C'))
+        ])
+    return d
+
+def seq_summary(nucleotide_count):
+    """
+    (dict) -> str
+
+    Returns a summary of theljkgjcgh
+    """
+    full_length = sum(nucleotide_count.values())
+    a = round(nucleotide_count['A'] * 100 / full_length)
+    t = round(nucleotide_count['T'] * 100 / full_length)
+    g = round(nucleotide_count['G'] * 100 / full_length)
+    c = round(nucleotide_count['C'] * 100 / full_length)
+    
+    summary = "Summary: Full Length({}bp) | A({}% {}) | T({}% {}) | G({}% {}) | C({}% {})".format(
+        full_length, a, nucleotide_count['A'], t, nucleotide_count['T'], g, nucleotide_count['G'], c, nucleotide_count['C'],)
+    
+    return summary
+
+
+
+##### Page Title + Text
 image = Image.open('GC-content_calculator.jpeg')
 
 st.image(image, use_column_width=True)
 
 st.write("""
 # GC Content Calculator Web App
+***
+What is GC Content?
+ 
+GC content percentage is calculated as Count(G + C)/Count(A + T + G + C) * 100%.
 
-This app calculates the GC content of query DNA! XXXXXXX
+The GC pair is bound by three hydrogen bonds, while AT pairs are bound by two hydrogen bonds.
+This means that the GC content affects the stability of DNA molecules, the secondary structure of mRNAs
+and the annealing temperature for primers and template DNA in PCR experiments.
 
+Knowing the GC-content of a DNA region is useful when designing primers for PCR experiments,
+since a higher GC-content level indicates a relatively higher melting temperature.
 ***
 """)
 
-# Input Text Box
-
+##### Input Text Box
 #st.sidebar.header('Enter DNA sequence')
 st.header('Enter DNA sequence')
 st.subheader('Paste sequence in FASTA format.')
-st.write('Base Type: Only accept four letters ATGC (case-insensitive)')
+st.write('Base Type: Only accepts four letters ATGC (case-insensitive)')
 st.write('Window Size: 30 nucleotides')
 
 sequence_input = """>NM_119948.4 Arabidopsis thaliana phosphoenolpyruvate carboxykinase 1 (PCK1), mRNA
@@ -74,7 +128,7 @@ AATGATGATGACTATATGTAATTTATTACTATCCATCGTCATATATTTTTGTTGGGCCTTTGCCAACATT
 TACATGATAGAACCAAGTACGAATAATATAAATTCTGGTCCAATCTGATGATGATTTTCAAAA
 """
 
-# sequence = st.sidebar.text_area("Sequence input", sequence_input, height=250)
+# remove header
 sequence = st.text_area("Sequence input", sequence_input, height=250)
 sequence = sequence.splitlines()
 sequence = sequence[1:] # Skips the sequence name (first line)
@@ -83,37 +137,6 @@ sequence = ''.join(sequence) # Concatenates list to string
 st.write("""
 ***
 """)
-
-#####################################
-# What is GC Content?
-
-# GC content is usually calculated as a percentage value and sometimes called G+C ratio or GC-ratio. GC-content percentage is calculated as Count(G + C)/Count(A + T + G + C) * 100%.
-
-# Why to care about GC Content?
-
-# The GC pair is bound by three hydrogen bonds, while AT pairs are bound by two hydrogen bonds. And so,
-# The GC content affects the stability of DNA.
-# The GC content affects the secondary structure of mRNA.
-# The GC content affects the annealing temperature for template DNA in PCR experiments.
-# Where to apply the GC Content?
-
-# The GC Content can be used in
-# 1. primer design for PCR experiments
-# 2. gene design from protein expression
-# 3. mRNA hairpin prediction
-# and etc.
-
-#####################################
-# Count(G + C)/Count(A + T + G + C) * 100%
-def gc_content(window):
-    """
-    (str) -> int
-    Returns the GC content of window (a DNA or RNA sequence).
-    """
-    gc = window.count('G') + window.count('C')
-    atgc = window.count('A') + window.count('T') + window.count('G') + window.count('C')
-    
-    return (gc/atgc) * 100
 
 # "Walk" over the sequence and calculate the GC content for each 30 nucleotide window
 all_gc = []
@@ -124,57 +147,46 @@ for i in range(len(sequence) - 30):
     all_gc.append(gc_cont)
     all_windows.append(seq)
 
-# create GC content distribution graph
+##### create GC content distribution graph
 window_num = np.arange(1, len(all_gc)+1)
 df_gc = pd.DataFrame({
     'window_num': window_num,
     'gc': all_gc
     })
+
+# plot
 gc_plot = alt.Chart(df_gc).mark_line(point=True).encode(x='window_num', y='gc')   
 st.write(gc_plot)
 
+##### Display Summary:
+nucleotide_count = DNA_nucleotide_count(sequence)
+s = seq_summary(nucleotide_count)
+st.write(s)
+st.write("""***""")
+# add something like this:
+# Summary: Full Length(2583bp) | A(28% 724) | T(29% 739) | G(23% 598) | C(20% 522)
 
 
-# Display GC Content vs Window number
-st.subheader('Window number vs GC content')
+##### Display DataFrame: GC Content vs Window number
+st.subheader('GC content for each window:')
 # df = pd.DataFrame.from_dict(X, orient='index')
 # df = df.rename({0: 'count'}, axis='columns')
 # df.reset_index(inplace=True)
 # df = df.rename(columns = {'index':'nucleotide'})
 
-################## round(x, 2)
-
-# h_letters = [ letter for letter in 'human' ]
-
-# all_gc_round = [round(x, 2) for x in all_gc] 
-
-all_gc_round = []
-for num in all_gc:
-    all_gc_round.append(round(num, 2)
-
-df_gc_windows = pd.DataFrame({
-    'window_num': window_num,
-    'gc': all_gc_round,
-    'window': all_windows
-    })
+all_gc_round = [round(x, 2) for x in all_gc] 
+gc_windows = {'Window #': window_num,
+              'GC content': all_gc_round,
+              'Sequence': all_windows}
+df_gc_windows = pd.DataFrame.from_dict(gc_windows)
 
 st.write(df_gc_windows)
 
-### 1. Print dictionary
-st.subheader('1. Print dictionary')
-def DNA_nucleotide_count(seq):
-  d = dict([
-            ('A',seq.count('A')),
-            ('T',seq.count('T')),
-            ('G',seq.count('G')),
-            ('C',seq.count('C'))
-            ])
-  return d
 
-X = DNA_nucleotide_count(sequence)
-### 3. Display DataFrame
+
+### 3. Display DataFrame   REMOVE?????????????
 st.subheader('3. Display DataFrame')
-df = pd.DataFrame.from_dict(X, orient='index')
+df = pd.DataFrame.from_dict(nucleotide_count, orient='index')
 df = df.rename({0: 'count'}, axis='columns')
 df.reset_index(inplace=True)
 df = df.rename(columns = {'index':'nucleotide'})
@@ -192,12 +204,31 @@ st.write(p)
 
 
 
-# add something like this:
-# Summary: Full Length(2583bp) | A(28% 724) | T(29% 739) | G(23% 598) | C(20% 522)
-st.write("""
-Summary: Full Length(2583bp) | A(28% 724) | T(29% 739) | G(23% 598) | C(20% 522)
-***
-""")
 
 
-# gc content distribution graph
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
